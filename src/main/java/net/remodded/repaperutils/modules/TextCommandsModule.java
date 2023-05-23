e1d31f46e6c699613137d82d08ac84ba67c5dced
Twarug <patryk280502@gmail.com>
2023-05-23 22:32:26 +0200

    Move TextCommandsModule config to main config

--- src/main/java/net/remodded/repaperutils/modules/TextCommandsModule.java
+++ src/main/java/net/remodded/repaperutils/modules/TextCommandsModule.java
@@ -8,18 +8,11 @@ import net.remodded.repaperutils.utils.PluginModule;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TextCommandsModule extends PluginModule<RePaperUtils> {
-
-    private static FileConfiguration commandsConfig;
-
     private final List<Command> registeredCommands = new ArrayList<>();
 
     public TextCommandsModule(RePaperUtils plugin) {
@@ -27,8 +20,9 @@ public class TextCommandsModule extends PluginModule<RePaperUtils> {
     }
 
     public boolean init() {
-        for (String commandName : commandsConfig.getKeys(false)) {
-            Command command = new TextCommand(commandName, commandsConfig.getStringList(commandName));
+        ConfigurationSection commands = config.getConfigurationSection("commands");
+        for (String commandName : commands.getKeys(false)) {
+            Command command = new TextCommand(commandName, commands.getStringList(commandName));
             if(CommandsUtils.register(plugin, command))
                 registeredCommands.add(command);
         }
@@ -48,19 +42,7 @@ public class TextCommandsModule extends PluginModule<RePaperUtils> {
     public void setupConfig(ConfigurationSection config) {
         super.setupConfig(config);
 
-        File configFile = new File(RePaperUtils.INSTANCE.getDataFolder(), "textCommands.yml");
-        commandsConfig = YamlConfiguration.loadConfiguration(configFile);
-
-        // Default commandsConfig
-        commandsConfig.addDefault("pomoc", ImmutableList.of("&8### &a&1POMOC &8### "));
-
-        commandsConfig.options().copyDefaults(true);
-        try {
-            commandsConfig.save(configFile);
-        } catch (IOException ex) {
-            error("Could not save config to " + configFile);
-            ex.printStackTrace();
-        }
+        config.addDefault("commands.pomoc", ImmutableList.of("&8### &a&1POMOC &8### "));
     }
 
     private static class TextCommand extends Command {
