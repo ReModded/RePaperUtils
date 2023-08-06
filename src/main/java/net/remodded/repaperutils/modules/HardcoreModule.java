118cc71006d3b1c01a8d57ab593973ca2e7bcbca
Twarug <patryk280502@gmail.com>
2023-08-06 16:59:56 +0200

    Add more configuration for HardcoreModule

--- src/main/java/net/remodded/repaperutils/modules/HardcoreModule.java
+++ src/main/java/net/remodded/repaperutils/modules/HardcoreModule.java
@@ -3,6 +3,7 @@ package net.remodded.repaperutils.modules;
 import net.remodded.repaperutils.RePaperUtils;
 import net.remodded.repaperutils.utils.PluginModule;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -13,26 +14,47 @@ import java.util.Random;
 public class HardcoreModule extends PluginModule<RePaperUtils> {
     private final Random random = new Random();
 
+    private boolean blockAnimalBreading;
+    private boolean blockChickenEggSpawn;
+    
+    private boolean slowCropsGrowth;
+    private boolean blockPlantGrowthWithoutSky;
+    
     public HardcoreModule(RePaperUtils plugin) {
         super("Hardcore", plugin);
     }
 
     @Override
     protected boolean init() {
+        blockAnimalBreading = config.getBoolean("blockAnimalBreading", true);
+        blockChickenEggSpawn = config.getBoolean("blockChickenEggSpawn", true);
+        
+        slowCropsGrowth = config.getBoolean("slowCropsGrowth", true);
+        blockPlantGrowthWithoutSky = config.getBoolean("blockPlantGrowthWithoutSky", true); 
         return true;
     }
 
     @Override
     protected void deinit(boolean doReload) {}
 
+    @Override
+    public void setupConfig(ConfigurationSection config) {
+        config.addDefault("blockAnimalBreading", true);
+        config.addDefault("blockChickenEggSpawn", true);
+        
+        config.addDefault("slowCropsGrowth", true);
+        config.addDefault("blockPlantGrowthWithoutSky", true);
+    }
+
     @EventHandler
     private void onAnimalBreed(EntityEnterLoveModeEvent ev) {
-        ev.setCancelled(true);
+        if (blockAnimalBreading)
+            ev.setCancelled(true);
     }
 
     @EventHandler
     private void onChickenSpawn(CreatureSpawnEvent ev) {
-        if(ev.getSpawnReason() == CreatureSpawnEvent.SpawnReason.EGG)
+        if(blockChickenEggSpawn && ev.getSpawnReason() == CreatureSpawnEvent.SpawnReason.EGG)
             ev.setCancelled(true);
     }
 
@@ -41,13 +63,13 @@ public class HardcoreModule extends PluginModule<RePaperUtils> {
         Block block = ev.getBlock();
 
         // Sky blocking
-        if(block.getLightFromSky() < 15) {
+        if(blockPlantGrowthWithoutSky && block.getLightFromSky() < 15) {
             ev.setCancelled(true);
             return;
         }
 
         // Slow grow speed x4
-        if(random.nextFloat() > 0.25f)
+        if(slowCropsGrowth && random.nextFloat() > 0.25f)
             ev.setCancelled(true);
     }
 }
